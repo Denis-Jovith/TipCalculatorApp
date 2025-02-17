d987c50d3b7c7e8cc68ab878f5d8ae4d8e1ed11a
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-17 14:16:37 +0300

    Made the TextField fully functioning

--- app/src/main/java/com/example/tiptimecalculator/MainActivity.kt
+++ app/src/main/java/com/example/tiptimecalculator/MainActivity.kt
@@ -80,6 +80,7 @@ fun EditNumberField(modifier: Modifier = Modifier){
     )
 }
 
+
 private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
     val tip = tipPercent / 100 * amount
     return NumberFormat.getCurrencyInstance().format(tip)
