11ab055eb7d8c77d00735ffd2b3f479f7c2d3370
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-02-17 15:48:45 +0300

    Used  NumberPassword Keyboard instead of Number

--- app/src/main/java/com/example/tiptimecalculator/MainActivity.kt
+++ app/src/main/java/com/example/tiptimecalculator/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -23,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiptimecalculator.ui.theme.TipTimeTheme
@@ -70,14 +72,15 @@ fun TipTimeLayout() {
 }
 
 @Composable
-fun EditNumberField(modifier: Modifier = Modifier){
-    var amountInput = remember { mutableStateOf("")  }
+fun EditNumberField(modifier: Modifier = Modifier) {
+    var amountInput = remember { mutableStateOf("") }
     TextField(
-        label = { Text(stringResource(R.string.bill_amount))},
         value = amountInput.value,
-        onValueChange = {amountInput.value = it},
-        modifier = modifier,
-        singleLine = true
+        onValueChange = { amountInput.value = it },
+        singleLine = true,
+        label = { Text(stringResource(R.string.bill_amount)) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+        modifier = modifier
     )
 }
 
